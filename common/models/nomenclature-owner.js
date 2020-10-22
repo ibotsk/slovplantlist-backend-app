6401c7e18b001dd8c65203d65a4bb5d19dc11729
Matus Kempa <matus.kempa@gmail.com>
2020-10-22 15:04:48 +0200

    Underscore properties changed to camelCase

--- common/models/nomenclature-owner.js
+++ common/models/nomenclature-owner.js
@@ -1,25 +1,26 @@
 'use strict';
 
 module.exports = function (Nomenclatureowner) {
+  Nomenclatureowner.disableRemoteMethodByName('create');     // Removes (POST) /products
+  Nomenclatureowner.disableRemoteMethodByName('upsert');     // Removes (PUT) /products
+  Nomenclatureowner.disableRemoteMethodByName('deleteById'); // Removes (DELETE) /products/:id
+  Nomenclatureowner.disableRemoteMethodByName('updateAll');  // Removes (POST) /products/update
+  Nomenclatureowner.disableRemoteMethodByName('prototype.updateAttributes');
+  // Removes (PUT) /products/:id
+  Nomenclatureowner.disableRemoteMethodByName('createChangeStream');
+  // Removes (GET|POST) /products/change-stream
 
-    Nomenclatureowner.disableRemoteMethodByName('create');     // Removes (POST) /products
-    Nomenclatureowner.disableRemoteMethodByName('upsert');     // Removes (PUT) /products
-    Nomenclatureowner.disableRemoteMethodByName('deleteById'); // Removes (DELETE) /products/:id
-    Nomenclatureowner.disableRemoteMethodByName('updateAll');  // Removes (POST) /products/update
-    Nomenclatureowner.disableRemoteMethodByName('prototype.updateAttributes');
-    // Removes (PUT) /products/:id
-    Nomenclatureowner.disableRemoteMethodByName('createChangeStream');
-    // Removes (GET|POST) /products/change-stream
-
-    Nomenclatureowner.observe('loaded', (ctx, next) => {
-        const data = ctx.data;
-        if (data.owner_ids) {
-            data.owner_ids = data.owner_ids.split(',');
-        }
-        if (data.owner_names) {
-            data.owner_names = data.owner_names.split(',');
-        }
-        next();
-    });
-
+  Nomenclatureowner.observe('loaded', (ctx, next) => {
+    const data = ctx.data;
+    if (data) {
+      const { ownerIds, ownerNames } = data;
+      if (ownerIds) {
+        data.ownerIds = ownerIds.split(',');
+      }
+      if (ownerNames) {
+        data.ownerNames = ownerNames.split(',');
+      }
+    }
+    next();
+  });
 };
